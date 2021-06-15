f8f159ff60c4a2cd93b3e22ccb029cb4ec98b8ac
Jim <jimbienvenu@gmail.com>
2021-06-16 01:40:09 +0200

    feat: Add currency field

--- src/infrastructure/dao/adapter/ProductMemoryAdapter.ts
+++ src/infrastructure/dao/adapter/ProductMemoryAdapter.ts
@@ -1,9 +1,17 @@
 import ProductMemory from "../model/ProductMemory";
 import IProduct from "../../../domain/model/IProduct";
 import Product from "../../../domain/model/Product";
+import {Money} from "../../../domain/types/Money";
 
 export default class ProductMemoryAdapter {
     static productMemoryToProduct(productMemory: ProductMemory): IProduct {
-        return new Product(productMemory.id, productMemory.name, productMemory.description, productMemory.price, productMemory.categoryId, productMemory.detail);
+        return new Product(
+            productMemory.id,
+            productMemory.name,
+            productMemory.description,
+            new Money(productMemory.price, productMemory.currency),
+            productMemory.categoryId,
+            productMemory.detail
+        );
     }
 }
--- src/infrastructure/dao/model/ProductMemory.ts
+++ src/infrastructure/dao/model/ProductMemory.ts
@@ -5,14 +5,24 @@ export default class ProductMemory {
     name: string;
     description: string;
     price: number;
+    currency: string;
     categoryId: number;
     detail: Detail[];
 
-    constructor(id: number, name: string, description: string, price: number, categoryId: number, detail: Detail[]) {
+    constructor(
+        id: number,
+        name: string,
+        description: string,
+        price: number,
+        currency: string,
+        categoryId: number,
+        detail: Detail[]
+    ) {
         this.id = id;
         this.name = name;
         this.description = description;
         this.price = price;
+        this.currency = currency;
         this.categoryId = categoryId;
         this.detail = detail;
     }
