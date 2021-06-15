4c61b92ea26e286a2a13546f0c9a279c2cc33d7e
Jim <jimbienvenu@gmail.com>
2021-06-15 21:55:56 +0200

    refactor: Use dateService instead of Date objects

--- src/useCases/ViewProductInformationAsUser.ts
+++ src/useCases/ViewProductInformationAsUser.ts
@@ -1,28 +1,28 @@
 import IProductRepository from "../domain/repository/IProductRepository";
-import IUserRepository from "../domain/repository/IUserRepository";
 import IOrderRepository from "../domain/repository/IOrderRepository";
-import IProduct from "../domain/models/IProduct";
+import IProduct from "../domain/model/IProduct";
+import IDateService from "../infrastructure/services/IDateService";
 
 export default class ViewProductInformationAsUser {
     constructor(
         private productRepository: IProductRepository,
-        private userRepository: IUserRepository,
-        private orderRepository: IOrderRepository
+        private orderRepository: IOrderRepository,
+        private dateService: IDateService,
     ) {
     }
 
     public execute(productId: number, userId: number): IProduct {
-        try {      
-            const user = this.userRepository.findOne(userId);
+        try {
             const product = this.productRepository.findOne(productId);
-
-            let currentDate = new Date();
-            currentDate.setMonth(currentDate.getMonth()-6);
-            const orderLastSixMonths = this.orderRepository.findByUserAndDate(userId,currentDate);
-
-            currentDate = new Date();
-            currentDate.setFullYear(currentDate.getFullYear()-1);
-            const orderLastYear = this.orderRepository.findByUserAndProductAndDate(userId,productId,currentDate);
+            const orderLastSixMonths = this.orderRepository.findByUserAndDate(
+                userId,
+                this.dateService.dateMinusMonths(6)
+            );
+            const orderLastYear = this.orderRepository.findByUserAndProductAndDate(
+                userId,
+                productId,
+                this.dateService.dateMinusYears(1)
+            );
 
             return product.evaluateDiscount(orderLastSixMonths, orderLastYear);
         } catch (e) {
